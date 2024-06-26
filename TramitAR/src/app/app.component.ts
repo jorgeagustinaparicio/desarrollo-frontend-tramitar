a6e344e9da2d65e3032314ed0ca5eea0eee49371
Franco Vilca <75865914+fra92nk@users.noreply.github.com>
2024-06-26 11:30:33 -0300

    navbar y footer configurados

--- TramitAR/src/app/app.component.ts
+++ TramitAR/src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from "./components/footer/footer.component";
+import { NavbarComponent } from "./components/navbar/navbar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [RouterOutlet, FooterComponent, NavbarComponent]
 })
 export class AppComponent {
   title = 'TramitAR';
--- TramitAR/src/app/app.routes.ts
+++ TramitAR/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { PanelComponent } from './components/panel/panel.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '', component: HomeComponent
+    },
+    {
+        path: 'panel', component: PanelComponent
+    }
+];
