080e13f803db2d3ba4d9d5e1f62e68a30dad6961
Franco Vilca <75865914+fra92nk@users.noreply.github.com>
2024-06-27 02:38:26 -0300

    Cambios varios

--- TramitAR/src/app/app.routes.ts
+++ TramitAR/src/app/app.routes.ts
@@ -7,6 +7,6 @@ export const routes: Routes = [
         path: '', component: HomeComponent
     },
     {
-        path: 'panel', component: PanelComponent
+        path: 'admin', component: PanelComponent
     }
 ];
--- TramitAR/src/app/components/home/home.component.ts
+++ TramitAR/src/app/components/home/home.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CardComponent } from "../card/card.component";
+import { CommonModule } from '@angular/common';
+import { Organismo } from '../../models/organismo.model';
+import { DataService } from '../../services/data.service';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
+  imports: [CardComponent, CommonModule]
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  organismos: Organismo[] = [];
 
+  constructor(private dataService: DataService) { }
+
+  ngOnInit(): void {
+    this.dataService.getOrganismos().subscribe(organismos => {
+      this.organismos = organismos;
+    });
+  }
 }
